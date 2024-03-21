d222de859f6beeaf787c23dd5d4b2424df3a67c5
GruppMatheus <matheus.grupp@pucpr.edu.br>
2024-03-20 21:36:07 -0300

    atualizei esse caralho

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+
+            System.out.println("Pagina de Cadastro");
+            System.out.println("Usuario:aaaaaa");
+            System.out.println("Senha:****");
+            System.out.println("CPF: 9999999999");
+            System.out.println("Hello");
+
+    }
+}
